6659a9858924089982c1e16bb3b8719c921373f1
Jez Higgins <jez@jezuk.co.uk>
2020-06-16 11:45:45 +0100

    feat(file-downloading): Wire up route into fastify

--- test/download-spec.js
+++ test/download-spec.js
@@ -2,19 +2,24 @@
 
 const PORT = 3003
 const HOST = '0.0.0.0'
-const expect = require('chai').expect
+const chai = require('chai')
+chai.use(require('chai-string'))
+const expect = chai.expect
 const tymly = require('@wmfs/tymly')
 const path = require('path')
+const axios = require('axios')
 
 describe('Download tests', function () {
   this.timeout(process.env.TIMEOUT || 5000)
 
   const secret = 'Shhh!'
   const audience = 'IAmTheAudience!'
+  const baseUrl = `http://localhost:${PORT}`
 
   const testFile = path.join(__dirname, 'fixtures', 'download', 'fixture.txt')
 
   let tymlyService, downloadService
+  let downloadPath
 
   before('start tymly', async () => {
     const tymlyServices = await tymly.boot(
@@ -33,12 +38,26 @@ describe('Download tests', function () {
 
     tymlyService = tymlyServices.tymly
     downloadService = tymlyServices.fileDownloading
+    const server = tymlyServices.server
+    server.listen(PORT, HOST, () => {
+      console.log(`Listening on ${PORT}`)
+    })
+
   })
 
   it ('add a file for download', () => {
-    const url = downloadService.addDownloadFile(testFile)
+    downloadPath = downloadService.addDownloadFile(testFile)
 
-    expect(url).to.include('/download/')
+
+    expect(downloadPath).to.include('/download/')
+  })
+
+  it ('download the file', async () => {
+    const download = await axios({
+      url: baseUrl + downloadPath,
+      method: 'GET'
+    })
+    expect(download.status).to.eql(200)
   })
 
   after('shutdown tymly', async () => {
